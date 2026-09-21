c33e744bfcc4c05e6a3beeca30572f3463840900
mkhalfan <mk5636@nyu.edu>
2026-09-21 07:06:47 -0400

    Add original author attribution to each page (#27)
    Custom Sphinx extension that queries git history at build time to find
    the first commit author of each .rst file. Displays the author name
    next to "Edit on GitHub" in the breadcrumb bar, linked to their GitHub
    profile (noreply emails) or email address (personal emails).

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,9 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_panels']
+import sys, os
+sys.path.insert(0, os.path.abspath("_ext"))
+extensions = ['sphinx_panels', 'git_author']
 templates_path = ['_templates']
 exclude_patterns = []
 
