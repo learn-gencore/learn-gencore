e0bb1ab1593a5e290fc287447935ebc3c6608af8
Jayaram Radhakrishnan <97015642+jayramr@users.noreply.github.com>
2026-09-23 11:50:11 +0400

    Revert "Add original author attribution to each page (#27)" (#30)
    This reverts commit c33e744bfcc4c05e6a3beeca30572f3463840900.

--- source/conf.py
+++ source/conf.py
@@ -9,9 +9,7 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-import sys, os
-sys.path.insert(0, os.path.abspath("_ext"))
-extensions = ['sphinx_panels', 'git_author']
+extensions = ['sphinx_panels']
 templates_path = ['_templates']
 exclude_patterns = []
 
